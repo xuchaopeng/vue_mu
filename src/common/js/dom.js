b711f9fb68ecbf26c0cc46efeafe1f6f6300018b
m18770074875_1@163.com <m18770074875_1@163.com>
2018-10-16 00:11:50 +0800

    singer

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -11,3 +11,13 @@ export function hasClass(el,className){
 	let reg = new RegExp('(^|\\s)'+className+'(\\s|$)')
 	return reg.test(el.className)
 }
+
+export function getData(el,name,val){
+	const prefix = 'data-'
+	const dataName = prefix + name
+	if(val){
+		return el.setAttribute(dataName,val)
+	}else{
+		return el.getAttribute(dataName)
+	}
+}
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,16 +1,22 @@
 <template>
 	<div class="singer">
+		<ListView :data="singers"></ListView>
 	</div>
 </template>
 
 <script type="text/javascript">
 	import {getSingerList} from 'api/singer'
 	import {ERR_OK} from 'api/config'
+	import Singer from 'common/js/singer'
+	import ListView from 'base/listview/listview'
+
+	const HOT_NAME = '热门';
+	const HOT_SINGER_LEN = 10 ;
 
 	export default {
 		data(){
 			return {
-				singer:[]
+				singers:[]
 			}
 		},
 		created(){
@@ -20,13 +26,60 @@
 			_getSingerList(){
 				getSingerList().then((res)=>{
 					if(res.code === ERR_OK){
-						this.singer = res.singerList.data.singerlist
-						console.log(res);
+						this.singers = this._normalizeSinger(res.singerList.data.singerlist)
+						console.log(this.singers)
 					}
 				})
+			},
+			_normalizeSinger(list){
+				let map = {
+					hot:{
+						title:HOT_NAME,
+						items:[]
+					}
+				}
+				list.forEach((item,index) => {
+					if(index < HOT_SINGER_LEN){
+						map.hot.items.push(new Singer({
+							id:item.singer_mid,
+							name:item.singer_name,
+							avatar:item.singer_pic
+						}))
+					}
+					const key = item.country;
+					if(!map[key]){
+						map[key] = {
+							title:key,
+							items:[]
+						}
+					}
+					map[key].items.push(new Singer({
+						id:item.singer_mid,
+						name:item.singer_name,
+						avatar:item.singer_pic
+					}))
+				})
+				let ret = []
+				let hot = []
+				for(let key in map){
+					let val = map[key];
+					val.title === HOT_NAME ? hot.push(map[key]) : ret.push(map[key]);
+				}
+				return hot.concat(ret)
 			}
+
+		},
+		components:{
+			ListView
 		}
 	}
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+	.singer{
+		position: fixed;
+	    top: 88px;
+	    bottom: 0;
+	    width: 100%;
+	}
+</style>
